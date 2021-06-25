bd4023c635e3b8f881b477f0f3bb05af4cc462d5
SURAJ JADHAV <69894599+JadhavSuraj7rk@users.noreply.github.com>
2021-06-25 10:26:46 +0530

    ✨ New Feature: slider with smooth transistion

--- js/logic.js
+++ js/logic.js
@@ -2,6 +2,7 @@
 const next = document.querySelector(".next-⏭️");
 const prev = document.querySelector(".prev-⏮️");
 const slides = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
 let CurrentSlide = 0;  //variable use to monitor which slide(number) display on screen
 
 
@@ -15,28 +16,31 @@ function display (CurrentSlide) {
 	});
 	slides[CurrentSlide].style.display = 'flex';
 	
+	
 }
 
 // Whenever user click next button this function gets trigger as function gets triggered next slide appear on the screen
 function nextSlide () {
+	
 	CurrentSlide++;
     // if slide number(CurrentSlide ) greater than length of slides(2) then CurrentSlide get reset
 	if (CurrentSlide > slides.length - 1) {
 		CurrentSlide = 0;
 	}
 	display(CurrentSlide);
-    console.log("next");
+  
 }
 
 // Whenever user click next button this function gets trigger as function gets triggered previous slide appear on the screen
 function prevSlide () {
+	
 	CurrentSlide--;
     // if slide number(CurrentSlide ) less than zero then CurrentSlide is equal to length of slides
 	if (CurrentSlide < 0) {
 		CurrentSlide = slides.length - 1;
 	}
 	display(CurrentSlide);
-    console.log("prev");
+   
 }
 
 // EventListener
